a6b3981de73221a19c4eb36a8272c485440c7968
xmatuska <xmatuskam1@stuba.sk>
2021-04-13 22:53:31 +0200

    var variables

---
+++ src/main/java/sk/stuba/fei/uim/oop/Contact.java
@@ -0,0 +1,14 @@
+package sk.stuba.fei.uim.oop;
+
+public class Contact {
+
+    String firstName;
+    String surname;
+
+    Contact [] friends;
+
+    public Contact(String firstName, String surname) {
+        this.firstName = firstName;
+        this.surname = surname;
+    }
+}
--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,12 @@
 package sk.stuba.fei.uim.oop;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
+        Contact contact = new Contact("Matusko","Matuska");
+        var contact2 = new Contact("Igorko", "Najselko");
+        var cislo = 5;
+        cislo = contact2;
 
     }
 }
